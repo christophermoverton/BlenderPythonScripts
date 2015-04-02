fa0f6c7735ef6edfc81684aea8d1d97d63236559
Christopher <christophermoverton@gmail.com>
2015-04-01 23:14:28 -0500

    update

--- Mondrian2dsubdivisiongenerator.py
+++ Mondrian2dsubdivisiongenerator.py
@@ -1,4 +1,69 @@
 #Mondrian like subdivision generator
 import bpy
+import random
 dimx = 50
 dimy = 65
+totalnodes = 3
+meshName = "Mondrian"
+obName = "MondrianObj"
+me = bpy.data.meshes.new(meshName)
+ob = bpy.data.objects.new(obName, me)
+##track edges on mondrian generator and vertices
+nodes = {}
+nodepostoi = {}
+for i in range(0,totalnodes):
+   attr = {}
+   randposx = random.randint(0,dimx)
+   randposy = random.randint(0,dimy)
+   attr['position'] = (randposx,randposy)
+   attr['left'] = None
+   attr['right'] = None
+   attr['up'] = None
+   attr['down'] = None
+   nodes[i] = attr
+   nodepostoi[(randposx,randposy)] = i
+nodeposlist = list(nodepostoi.keys())   
+#node ordering
+nodexrank = nodeposlist[0:len(nodeposlist)]
+nodeyrank = nodeposlist[0:len(nodeposlist)]
+nodexrank.sort(key=lambda tup: tup[0])
+nodeyrank.sort(key=lambda tup: tup[1])
+nodexirank = []
+for pos in nodexrank:
+    nodexirank.append(nodepostoi[pos])
+nodeyirank = []
+for pos in nodeyrank:
+    nodeyirank.append(nodepostoi[pos])
+##set node crossings
+crossingnodes = {}
+crossingnodesrev = {}
+for node in nodes:
+    yrank = nodeyirank.index(node)
+    posx = nodes[node]['position'][0]
+    copynranks = nodeyirank[0:len(nodeyirank)]
+    del copynranks[yrank]
+    for cr in range(0,len(totalnodes)-1):
+        attr = {}
+        nodeval = yrank*10+copynranks[cr]
+        posy = nodes[copynranks[cr]]['position'][1]
+        attr['position'] = (posx,posy) 
+        attr['left'] = None
+        attr['right'] = None
+        nodevald = None
+        crnodepos = nodeyirank.index(copynranks[cr])
+        if crnodepos > 0:
+            if nodeyirank[crnodepos-1] == yrank:
+                nodevald = yrank
+            else:
+                nodevald = yrank*10 + nodeyirank[crnodepos-1]
+        attr['down'] = nodevald
+        nodevald = None
+        if crnodepos < len(nodeyirank)-1:
+            if nodeyirank[crnodepos+1] == yrank:
+                nodevald = yrank
+            else:
+                nodevald = yrank*10 + nodeyirank[crnodepos+1]
+        attr['top'] = nodevald
+#construct faces and vertices from nodes
+for node in nodes:
+    
