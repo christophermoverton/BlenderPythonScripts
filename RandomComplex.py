9671a4a5dc8bcaf99096db411efb9db265fbfc14
Christopher Overton <christophermoverton@gmail.com>
2015-05-05 08:32:51 -0500

    update

--- RandomComplex.py
+++ RandomComplex.py
@@ -23,6 +23,11 @@ def generateRandomVertex():
 def generateRandomVertexMM(minX,maxX,minY,maxY):
     return (random.uniform(minX,maxX),random.uniform(minY,maxY))
 
+def distance(a,b):
+    ax,ay = a
+    bx,by = b
+    return ((ax-bx)**2+(ay-by)**2)**.5
+
 def getMinMax(edge):
     edgex = edge[0:len(edge)]
     edgey = edge[0:len(edge)]
@@ -36,35 +41,112 @@ def getMinMax(edge):
     maxy = edgey[1][1]
     return (minx,maxx,miny,maxy)
 
+def updateEdges(a,b,edges,dedge,vedges):
+    edges.append((a,b))
+    d = distance(a,b)
+    if d in dedge:
+        dedge[d].append((a,b))
+    else:
+        dedge[d] = [(a,b)]
+    if a in vedges:
+        vedges[a].append((a,b))
+    else:
+        vedges[a] = [(a,b)]
+    if b in vedges:
+        vedges[b].append((a,b))
+    else:
+        vedges[b] = [(a,b)]
+
+def deleteEdge(edge,edges,dedge,vedges):
+    a,b = edge
+    d = distance(a,b)
+    edges.remove(edge)
+    dedge[d].remove(edge)
+    if len(dedge[d]) == 0:
+        del dedge[d]
+    print(edge)
+    print(vedges)
+    vedges[a].remove(edge)
+    vedges[b].remove(edge)
+
 edgecount = 0
 vertices = []
 edges = []
+dedge = {}
+vedges = {}
+edged = {}
 for i in range(0,3):
     vertices.append(generateRandomVertex())
     if len(vertices) > 0 and len(vertices) != 1:
-        edges.append((vertices[0],vertices[len(vertices)-1]))
+        a = vertices[0]
+        b = vertices[len(vertices)-1]
+        updateEdges(a,b,edges,dedge,vedges)
+##        edges.append((a,b))
+##        d = distance(a,b)
+##        if d in dedge:
+##            dedge[d].append((a,b))
+##        else:
+##            dedge[d] = [a,b]
+##        if a in vedges:
+##            vedges[a].append((a,b))
+##        else:
+##            vedges[a] = [(a,b)]
+##        if b in vedges:
+##            vedges[b].append((a,b))
+##        else:
+##            vedges[a] = [(a,b)]
     edgecount += 1
-
-edges.append((vertices[2],vertices[1]))
+a = vertices[2]
+b = vertices[1]
+updateEdges(a,b,edges,dedge,vedges)
+##edges.append((vertices[2],vertices[1]))
+##d = distance(a,b)
+##if d in dedge:
+##    dedge[d].append((a,b))
+##else:
+##    dedge[d] = [a,b]
+##if a in vedges:
+##    vedges[a].append((a,b))
+##else:
+##    vedges[a] = [(a,b)]
+##if b in vedges:
+##    vedges[b].append((a,b))
+##else:
+##    vedges[a] = [(a,b)]
+edgecount += 1
 
 Q = []
+edgec = 0
+print(dedge)
 while (edgecount < PolygonSize+1):
     if len(Q) == 0:
        ##fill Q
-       edgescopy = edges[0:len(edges)]
-       random.shuffle(edgescopy)
-       Q = edgescopy
-    pedge = Q[0]
+       ##edgescopy = edges[0:len(edges)]
+       ##random.shuffle(edgescopy)
+       edgekeys = dedge.keys()
+       edgekeys.sort()
+       Q = edgekeys
+    qedges = dedge[Q[0]][0:len(dedge[Q[0]])]   
+    pedge = dedge[Q[0]][edgec]
     minx,maxx,miny,maxy = getMinMax(pedge)
     nvert = generateRandomVertexMM(minx,maxx,miny,maxy)
     vertices.append(nvert)
-    nedge1 = (pedge[0],nvert)
-    nedge2 = (pedge[1],nvert)
-    pedgei = edges.index(pedge)
-    del edges[pedgei]
-    edges.append(nedge1)
-    edges.append(nedge2)
-    del Q[0]
+    updateEdges(pedge[0],nvert,edges,dedge,vedges)
+    updateEdges(pedge[1],nvert,edges,dedge,vedges)
+    ##nedge1 = (pedge[0],nvert)
+    ##nedge2 = (pedge[1],nvert)
+##    pedgei = edges.index(pedge)
+##    del edges[pedgei]
+    deleteEdge(pedge,edges,dedge,vedges)
+    ##edges.append(nedge1)
+    ##edges.append(nedge2)
+    ##del Q[0]
     edgecount += 1
-    
-    
+    print('Q[0]', Q[0])
+    print('qedges', qedges)
+    print('length qedges - 1: ',len(qedges)-1)
+    if edgec == len(qedges)-1:
+        del Q[0]
+        edgec = 0
+    else:    
+        edgec += 1
